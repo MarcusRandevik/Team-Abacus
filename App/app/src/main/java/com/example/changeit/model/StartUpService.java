c2c35ff2839f854e13a0f0d1ef40f4c3f47bed69
Moa Berglund <moa.berglund00@hotmail.com>
2021-04-14 17:01:56 +0200

    javadoc

--- App/app/src/main/java/com/example/changeit/model/StartUpService.java
+++ App/app/src/main/java/com/example/changeit/model/StartUpService.java
@@ -1,5 +1,10 @@
 package com.example.changeit.model;
-
+/**
+ * @author Moa Berglund, Noa Tholén, Lisa Samuelsson
+ * Uses UserHandler
+ * Represents an start up service for the application. Initializes hard coded values to the applicatione
+ * @since 2020-04-13
+ */
 public class StartUpService {
 
     public static void loadPackage() { //kalla på i "main"
