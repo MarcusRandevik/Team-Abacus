f41cb8db13dbed1f64f1b03b29867237cec43e8b
Marcus Randevik <marcus.randevik@hotmail.se>
2021-04-21 08:22:32 +0200

    Filter now works for rooms

--- App/app/src/main/java/com/example/changeit/ui/home/ListFilterFragment.java
+++ App/app/src/main/java/com/example/changeit/ui/home/ListFilterFragment.java
@@ -8,18 +8,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.changeit.R;
 import com.example.changeit.databinding.FragmentFilterBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.slider.Slider;
 
 public class ListFilterFragment extends BottomSheetDialogFragment {
+
+    private HomeViewModel homeViewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+
         FragmentFilterBinding fragmentFilterBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_filter, container, false);
+
+        fragmentFilterBinding.slider.setValue(homeViewModel.getMaxRooms());
+
+        fragmentFilterBinding.slider.addOnChangeListener((slider, value, fromUser) -> {
+            homeViewModel.setMaxRooms(Math.round(value));
+        });
+
         return fragmentFilterBinding.getRoot();
     }
 }
